2f6fbc271c48d18c078596af1e63b8e9f4a172e5
ramu singh <ramusingh72103@gmail.com>
2024-03-20 12:38:42 +0530

    added log statement

--- src/main/java/com/nrt/controller/HomeController.java
+++ src/main/java/com/nrt/controller/HomeController.java
@@ -8,6 +8,7 @@ public class HomeController {
 
 	@GetMapping("/test")
 	public String testMethod() {
+		System.out.println("method called from api");
 		return "Response Message from service";
 	}
 }
